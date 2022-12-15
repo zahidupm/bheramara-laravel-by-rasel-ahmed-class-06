bf2db7197e83c5c6ab5b71c08ff66281eb2a772a
zahidupm <zahidhasanlimon111@gmail.com>
2022-12-15 15:22:11 +0600

    dashboard

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,3 +1,8 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Posts') }}
+        </h2>
+    </x-slot>
     <h2>Posts</h2>
 </x-app-layout>
